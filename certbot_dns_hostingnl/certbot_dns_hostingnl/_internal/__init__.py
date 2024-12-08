364ea38f88a4ae14e63e2726f5565833f844ecb0
Jordy Kuhne <jordy.kuhne@gmail.com>
2024-12-08 09:29:11 +0100

    cleanup

--- certbot_dns_hostingnl/certbot_dns_hostingnl/_internal/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Internal implementation of `~certbot_dns_hostingnl.dns_hostingnl` plugin."""
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='certbot-dns-hostingnl',
     install_requires=[
         'certbot',
     ],
+    packages=find_packages(),
     entry_points={
         'certbot.plugins': [
-            'dns-hostingnl = certbot_dns_hostingnl._internal.dns_hostingnl:Authenticator',
+            'dns-hostingnl = certbot_dns_hostingnl.dns_hostingnl:Authenticator',
         ],
     },
 )
